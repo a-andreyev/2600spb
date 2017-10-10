c9db77327c55c82829be7af7eb0243fc06a9ea8f
z4fd <zafod@protonmail.com>
2017-10-10 18:23:09 +0300

    Removed Google analytics

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-6227584-29']);
-  _gaq.push(['_setDomainName', '.2600.nl']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
 window.requestAnimFrame = (function(callback){
     return window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
